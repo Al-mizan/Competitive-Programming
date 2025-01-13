f257689041db28e32ce3ea465164cfc72362d661
Al-mizan <mdalmizanakon@gmail.com>
2025-01-13 23:42:04 +0600

    solved some problems and participated a contest

---
+++ codeforces/contests/Round 996 (Div. 2)/B_Crafting.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+#define endl            '\n'
+#define ff              first
+#define ss              second
+#define pi              acos(-1)
+#define mod             1000000007 
+#define newline         cout << '\n'
+#define no              cout << "NO\n"
+#define yes             cout << "YES\n"
+#define all(A)          A.begin(), A.end()
+#define rall(x)         x.rbegin(), x.rend()
+#define mem(a,n)        memset(a, n, sizeof(a))
+#define setPre(n)       fixed << setprecision(n)
+#define cases(tc)       cout << "Case #" << tc << ": "
+#define debug(args...)  cout << #args << " = " << args << endl;
+template<typename T>istream &operator>>(istream &istream,vector<T>&v){for(auto &it:v)cin>>it;return istream;}
+template<typename T>ostream &operator<<(ostream &ostream,const vector<T>&c){for(auto &it:c)cout<<it<<' ';return ostream;}
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> v(n), r(n);
+    cin >> v >> r;
+    
+    ll neg = 0, mini  = INT_MAX, nn = 0;
+    for(int i=0; i<n; i++) {
+        ll diff = v[i] - r[i];
+        if(diff < 0) {
+            neg++;
+            nn = -1*diff;
+        }
+        else mini = min(mini, diff);
+    }
+    // debug(mini);
+    // debug(nn);
+    if(neg <= 1 && mini >= nn){
+        yes;
+    }
+    else no;
+}
+
+int32_t main()
+{
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        // cases(tc);
+        solve();
+    }
+    return 0;
+}
