71fa7a7a0675bbed99bb14b1cdfdb547d28a0c46
Al-mizan <mdalmizanakon@gmail.com>
2025-02-11 16:29:37 +0600

    solve some problems

---
+++ codeforces/practice/B_Candies.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+#define endl            '\n'
+#define inf             1e18
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define mod             1000000007 
+#define newline         cout << '\n'
+#define no              cout << "NO\n"
+#define yes             cout << "YES\n"
+#define all(A)          A.begin(), A.end()
+#define rall(A)         A.rbegin(), A.rend()
+#define mem(A,n)        memset(A, n, sizeof(A))
+#define debug(args...)  cout << #args << " = " << args << endl;
+template<typename T>istream &operator>>(istream &istream,vector<T>&v){for(auto &it:v)cin>>it;return istream;}
+template<typename T>ostream &operator<<(ostream &ostream,const vector<T>&c){for(auto &it:c)cout<<it<<' ';return ostream;}
+
+void solve() {
+    ll n;
+    cin >> n;
+    vector<ll> v;
+
+    if(n%2) {
+        while(n>1) {
+            n /= 2;
+            if(n%2) v.pb(2);
+            else {
+                v.pb(1);
+                n++;
+            }
+        }
+        reverse(all(v));
+        cout << v.size() << endl;
+        cout << v << endl;
+    } 
+    else {
+        cout << -1 << endl;
+    }
+}
+
+int32_t main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
---
+++ codeforces/practice/E_Living_Sequence.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+#define endl            '\n'
+#define inf             1e18
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define mod             1000000007 
+#define newline         cout << '\n'
+#define no              cout << "NO\n"
+#define yes             cout << "YES\n"
+#define all(A)          A.begin(), A.end()
+#define rall(A)         A.rbegin(), A.rend()
+#define mem(A,n)        memset(A, n, sizeof(A))
+#define debug(args...)  cout << #args << " = " << args << endl;
+template<typename T>istream &operator>>(istream &istream,vector<T>&v){for(auto &it:v)cin>>it;return istream;}
+template<typename T>ostream &operator<<(ostream &ostream,const vector<T>&c){for(auto &it:c)cout<<it<<' ';return ostream;}
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    string s = "";
+    while(n>0) {
+        ll rem = n%9;
+        if(rem < 4) s += '0' + rem;
+        else s += rem+'1';
+        n /= 9;
+    }
+    reverse(all(s));
+
+    cout << s << endl;
+}
+
+int32_t main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1;
+    cin >> t;
+    while(t--) {
+        solve();
+    }
+    return 0;
+}
