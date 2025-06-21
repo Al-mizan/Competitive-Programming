9aeca6b26250be64657fb576414dfd47344d99d9
Al-mizan <mdalmizanakon@gmail.com>
2025-06-21 17:16:11 +0600

    solved a cses problem

---
+++ cses/Digit_Queries.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ull = unsigned long long;
+#define endl            '\n'
+#define inf             1e18
+#define ff              first
+#define ss              second
+#define pb              push_back
+#define mod             1000000007 
+#define newline         cout << '\n'
+#define no              cout << "NO\n"
+#define yes             cout << "YES\n"
+#define all(A)          A.begin(), A.end()
+#define rall(A)         A.rbegin(), A.rend()
+#define mem(A,n)        memset(A, n, sizeof(A))
+#define debug(args...)  cout << #args << " = " << args << endl;
+template<typename T>istream &operator>>(istream &istream,vector<T>&v){for(auto &it:v)cin>>it;return istream;}
+template<typename T>ostream &operator<<(ostream &ostream,const vector<T>&c){for(auto &it:c)cout<<it<<' ';return ostream;}
+
+void solve() {
+    ll n;
+    cin >> n;
+    
+    ll digit = 1, count = 9, start = 1;
+    while (n > count*digit) {
+        n -= count*digit;
+        digit++;
+        start *= 10;
+        count *= 10;
+    }
+
+    start += (n - 1) / digit;
+    int pos = (n - 1) % digit;
+    string number = to_string(start);
+
+    cout << number[pos] << endl;
+}
+
+int32_t main() {
+    ios_base ::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t = 1; cin >> t;
+    while (t--) solve();
+    return 0;
+}
